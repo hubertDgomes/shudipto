e39ddad89280310aa5b36aef35631bc956e9f440
hubertDgomes <gomeshubert8@gmail.com>
2025-12-04 23:12:12 +0600

    Refactor navigation links to use anchor tags; update app.js to comment out event listeners

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-let home = document.querySelector("#home")
-let about  =document.querySelector("#about")
+// let home = document.querySelector("#home")
+// let about  =document.querySelector("#about")
 
-home.addEventListener("click", ()=>{
-    window.location.href = "/index.html"
-})
-about.addEventListener("click", ()=>{
-    window.location.href = "./AllHTMLFiles/about.html"
-})
+// home.addEventListener("click", ()=>{
+//     window.location.href = "/index.html"
+// })
+// about.addEventListener("click", ()=>{
+//     window.location.href = "./AllHTMLFiles/about.html"
+// })
