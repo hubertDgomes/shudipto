68bbd35b753ff1e26755f33c1c0d7e865d03df04
hubertDgomes <gomeshubert8@gmail.com>
2025-12-04 20:56:48 +0600

    First-Commit

---
+++ app.js
@@ -0,0 +1,9 @@
+let home = document.querySelector("#home")
+let about  =document.querySelector("#about")
+
+home.addEventListener("click", ()=>{
+    window.location.href = "./"
+})
+about.addEventListener("click", ()=>{
+    window.location.href = "about.html"
+})
