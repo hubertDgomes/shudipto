ea4271de91a11931e96948f6295de93389158c7c
hubertDgomes <gomeshubert8@gmail.com>
2025-12-04 22:48:36 +0600

    Add vision and mission sections to about.html; update styles for new content

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let home = document.querySelector("#home")
 let about  =document.querySelector("#about")
 
 home.addEventListener("click", ()=>{
-    window.location.href = "./index.html"
+    window.location.href = "/index.html"
 })
 about.addEventListener("click", ()=>{
     window.location.href = "/AllHTMLFiles/about.html"
