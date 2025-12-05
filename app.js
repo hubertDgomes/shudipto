800ab856d5c61c19498cb4bbe43d3ad4cf844d2f
hubertDgomes <gomeshubert8@gmail.com>
2025-12-05 12:40:58 +0600

    Add the sign up

--- app.js
+++ app.js
@@ -1,9 +1,67 @@
-// let home = document.querySelector("#home")
-// let about  =document.querySelector("#about")
+let formId = document.querySelector("#formId")
+let email = document.querySelector(".email")
+let name = document.querySelector(".name")
+let id = document.querySelector(".id")
+let date = document.querySelector(".date")
+let textarea = document.querySelector(".textarea")
 
-// home.addEventListener("click", ()=>{
-//     window.location.href = "/index.html"
-// })
-// about.addEventListener("click", ()=>{
-//     window.location.href = "./AllHTMLFiles/about.html"
-// })
+let emailError = document.querySelector(".emailError")
+let nameError = document.querySelector(".nameError")
+let idError = document.querySelector(".idError")
+let dateError = document.querySelector(".dateError")
+let areaError = document.querySelector(".areaError")
+
+
+
+
+formId.addEventListener("submit", (e) => {
+    e.preventDefault()
+    if(email.value == ""){
+        emailError.innerHTML = "Email is missing"
+    }
+    else{
+        emailError.innerHTML =""
+    }
+
+
+    if(name.value == ""){
+        nameError.innerHTML = "Name is missing"
+    }
+    else{
+        nameError.innerHTML =""
+    }
+
+
+    if(id.value == ""){
+        idError.innerHTML = "ID is missing"
+    }
+    else{
+        idError.innerHTML =""
+    }
+
+
+    if(date.value == ""){
+        dateError.innerHTML = "Date is missing"
+    }
+    else{
+        dateError.innerHTML =""
+    }
+
+
+
+
+    if(textarea.value == ""){
+        areaError.innerHTML = "Kinldy write something"
+    }
+    else{
+        areaError.innerHTML =""
+    }
+
+
+    if(email.value && name.value && id.value && date.value && textarea.value){
+        window.location.href = "signup.html"
+    }
+
+
+    
+})
